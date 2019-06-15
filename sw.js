98256e80edead22b16196a4c7e94c506602c14fa
SirTapir <37269469+SirTapir@users.noreply.github.com>
2019-06-15 14:19:10 +0700

    checking offline functionality

--- sw.js
+++ sw.js
@@ -33,12 +33,6 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request).then(function(response) {
       // Fall back to network
       return response || fetch(event.request);
-    }).catch(function() {
-      // If both fail, show a generic fallback:
-      return caches.match('/offline.html');
-      // However, in reality you'd have many different
-      // fallbacks, depending on URL & headers.
-      // Eg, a fallback silhouette image for avatars.
     })
   );
 });
